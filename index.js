58c2b4dfda83bd110b9de46345b69da9913388db
Yusra Ahmad <yusraahmadaug@gmail.com>
2023-11-10 13:12:13 +0100

    play again function

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import promptSync from "prompt-sync";
 
 const prompt = promptSync();
 
-// Bulls and Cows
-
 // Random Secret Number generating Function
 function generatesSecretNum() {
   const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -19,23 +17,29 @@ function generatesSecretNum() {
 
 // Checking Vaild Name
 const validateName = (name) => {
-    if (name.length === 0) {
-      console.log(chalk.bgRedBright(`Name cannot be empty ${"🚫"}. Please enter your name.`));
-      return false;
-    }
-  
-    if (/[^A-Za-z\s]/.test(name)) {
-      console.log(chalk.bgRedBright(`Invalid name ${"🚫"}. Please use only letters and spaces.`));
-      return false;
-    }else if( name.length <= 4){
-        console.log(chalk.bgRedBright(`Invalid name ${"🚫"}. Please Enter proper name.`));
-        return false
-    }
-  
-    return true;
-  };
-  
+  if (name.length === 0) {
+    console.log(
+      chalk.bgRedBright(`Name cannot be empty ${"🚫"}. Please enter your name.`)
+    );
+    return false;
+  }
 
+  if (/[^A-Za-z\s]/.test(name)) {
+    console.log(
+      chalk.bgRedBright(
+        `Invalid name ${"🚫"}. Please use only letters and spaces.`
+      )
+    );
+    return false;
+  } else if (name.length <= 1) {
+    console.log(
+      chalk.bgRedBright(`Invalid name ${"🚫"}. Please Enter proper name.`)
+    );
+    return false;
+  }
+
+  return true;
+};
 
 // Checking the Input is Valid
 const validateInput = (input) => {
@@ -79,109 +83,121 @@ const getHint = (secretNumber, guess) => {
   return `${bulls} ${"Bull(s)🐃"} & ${cows} ${"Cow(s)🐄"}`;
 };
 
-
 // Extras
 // ADDING LEVELS TO Game
 const gameLevels = [
-    { name: "Easy", attempts: 10 },
-    { name: "Medium", attempts: 7 },
-    { name: "Hard", attempts: 4 },
-  ];
-  const chooseLevel =() =>{
-    console.log(chalk.magentaBright("Choose a game level:"));
-    for (let i = 0; i < gameLevels.length; i++) {
-      console.log(chalk.rgb(
-        34,
-        56,
-        402
-      )(`${i + 1}. ${gameLevels[i].name}`));
-    }
-  
-    while (true) {
-      const choice = prompt(chalk.rgb(
-        132,
-        160,
-        632
-      )(`Enter the number of the`) +  chalk.cyanBright(` level `)  + chalk.rgb(
-        132,
-        160,
-        632
-      )(`you want to play: `));
-      const selectedLevel = parseInt(choice);
-      
-      if (selectedLevel >= 1 && selectedLevel <= gameLevels.length) {
-        return gameLevels[selectedLevel - 1];
-      } else {
-        console.log(chalk.rgb(
-            234,
-            56,
-            42
-          )(`Invalid choice ${"🚫"}. Please select a valid level.`));
-      }
+  { name: "Easy", attempts: 10 },
+  { name: "Medium", attempts: 7 },
+  { name: "Hard", attempts: 4 },
+];
+const chooseLevel = () => {
+  console.log(chalk.magentaBright("Choose a game level:"));
+  for (let i = 0; i < gameLevels.length; i++) {
+    console.log(chalk.rgb(34, 56, 402)(`${i + 1}. ${gameLevels[i].name}`));
+  }
+
+  while (true) {
+    const choice = prompt(
+      chalk.rgb(132, 160, 632)(`Enter the number of the`) +
+        chalk.cyanBright(` level `) +
+        chalk.rgb(132, 160, 632)(`you want to play: `)
+    );
+    const selectedLevel = parseInt(choice);
+
+    if (selectedLevel >= 1 && selectedLevel <= gameLevels.length) {
+      return gameLevels[selectedLevel - 1];
+    } else {
+      console.log(
+        chalk.rgb(
+          234,
+          56,
+          42
+        )(`Invalid choice ${"🚫"}. Please select a valid level.`)
+      );
     }
   }
+};
 
 // ----------------------------------------------------------------------
 
 // Main GAME "FUNCTION"
 function PlayGame() {
-  let secretNumber = generatesSecretNum();
+  let isPlaying = true;
+  while (isPlaying) {
+    let secretNumber = generatesSecretNum();
 
-
-  console.log(chalk.cyanBright(`Welcome to Bulls & Cows!  `));
-  console.log(chalk.whiteBright(`${"🔎"} Instructions for Game
+    console.log(chalk.cyanBright(`Welcome to Bulls & Cows!  `));
+    console.log(
+      chalk.magenta(`${"🔎"} Instructions for Game: `) +
+        chalk.whiteBright(`
    ${"📌"} You're the guesser, and your task is to guess a 4-digit secret number with unique digits.
    ${"📌"} Type a 4-digit number with no repeated digits .
    ${"📌"} After each guess, you'll receive hints in the form of "bulls" (correct digits in the right position) and "cows" (correct digits in the wrong position).
    ${"📌"} You win when you guess all 4 digits correctly in the right positions.
-  `));
+  `)
+    );
 
-  let name;
-while(true) {
-    name =prompt(chalk.rgb(234, 56, 432)("What is your name? "));
-    if (validateName(name)){
-        console.log(chalk.rgb(503, 134, 898)(`Player's name  is: ${name}`));
+    let name;
+    while (true) {
+      name = prompt(chalk.rgb(234, 56, 432)("What is your name? "));
+      if (validateName(name)) {
+        console.log(chalk.rgb(503, 134, 898)(`Player's name  is: ${name}\n`));
         break;
-}
-}
+      }
+    }
 
-let attempts =0;
-let selectedLevel = chooseLevel();
-let totalAllowedAttempts = selectedLevel.attempts;
+    let attempts = 0;
+    let selectedLevel = chooseLevel();
+    let totalAllowedAttempts = selectedLevel.attempts;
 
-      console.clear()
-      while (attempts < totalAllowedAttempts) {
-        const guess = prompt(chalk.rgb(4,607,190)(` ${name} `) 
-        + chalk.green(`please guess A Number ${"➡"} `));
-        if (validateInput(guess)) {
-          if (guess !== secretNumber) {
-            attempts = attempts + 1;
-            console.log(chalk.magenta(`Remaining ${totalAllowedAttempts -attempts} Attempts\n`));
-            const hint = getHint(secretNumber, guess);
-            console.log(chalk.yellow(`Hint: ${hint}`));
-            if (attempts === totalAllowedAttempts) {
-              return chalk.bgBlack(
+    console.clear();
+    while (attempts < totalAllowedAttempts) {
+      const guess = prompt(
+        chalk.rgb(4, 607, 190)(` ${name} `) +
+          chalk.green(`please guess A Number ${"➡"} `)
+      );
+      if (validateInput(guess)) {
+        if (guess !== secretNumber) {
+          attempts = attempts + 1;
+          console.log(
+            chalk.magenta(
+              `Remaining ${totalAllowedAttempts - attempts} Attempts\n`
+            )
+          );
+          const hint = getHint(secretNumber, guess);
+          console.log(chalk.yellow(`Hint: ${hint}`));
+          if (attempts === totalAllowedAttempts) {
+            console.log(
+              chalk.bgBlack(
                 `You Lose ${"☹"} the Game ${"👎"} . The Secret Number is:  ${secretNumber}`
-              );
-            }
-          } else if (guess === secretNumber) {
-            return chalk.blue(
-              `${"🎊"} Congratulations! ${name}  ${"🥳"} guessed the secret number ${secretNumber} in ${attempts} attempts. ${"🎊"}`
+              )
             );
           }
-        } else {
+        } else if (guess === secretNumber) {
           console.log(
-            chalk.rgb(
-              234,
-              56,
-              42
-            )(
-              `${name} guess Invalid Number ${"🚫"}. Please enter a 4-digit number with unique digits.`
+            chalk.blue(
+              `${"🎊"} Congratulations! ${name}  ${"🥳"} guessed the secret number ${secretNumber} in ${attempts} attempts. ${"🎊"}`
             )
           );
         }
+      } else {
+        console.log(
+          chalk.rgb(
+            234,
+            56,
+            42
+          )(
+            `${name} guess Invalid Number ${"🚫"}. Please enter a 4-digit number with unique digits.`
+          )
+        );
       }
+    }
+    let playAgain = prompt(`Would you like to Play again? [y/n]`);
+    if (playAgain.toLowerCase() !== "y") {
+      return (isPlaying = false);
+    }
 
-
+    console.clear();
+  }
 }
-console.log(PlayGame());
+PlayGame();
